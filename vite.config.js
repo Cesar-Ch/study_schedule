560c0d331564a599491502d63f34e7d059cfdff8
Cesar-Ch <chafloque1530@gmail.com>
2025-07-10 17:56:23 -0500

    Remove PWA plugin configuration from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA(
-    {
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: "Study Schedule",
-        short_name: "Study Schedule",
-        description: "Study Schedule",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
-          {
-            "src": "/study_schedule/pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/study_schedule/pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/study_schedule/pwa-maskable-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "/study_schedule/pwa-maskable-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
-        ]
-      }
-    }
-  )],
+  plugins: [react(), tailwindcss()],
   base: "/study_schedule/"
 })
