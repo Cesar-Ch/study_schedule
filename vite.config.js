5f30a12bc10e061d9f765727fa022f7f39162f77
Cesar-Ch <chafloque1530@gmail.com>
2025-07-10 20:08:36 -0500

    Remove clean.html and main2.jsx files; update Vite configuration to simplify build setup

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/study_schedule/",
-  build: {
-    rollupOptions: {
-      input: {
-        main:  resolve(__dirname, 'clean.html')
-      }
-    }
-  }
+  base: "/study_schedule/"
 })
