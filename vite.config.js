8d4bad2bfad04c36c6298c348249c8bf81cb6c14
Cesar-Ch <chafloque1530@gmail.com>
2025-07-10 19:55:28 -0500

    Add clean.html and update build configuration for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/study_schedule/"
+  base: "/study_schedule/",
+  build: {
+    rollupOptions: {
+      input: {
+        main:  resolve(__dirname, 'clean.html')
+      }
+    }
+  }
 })
